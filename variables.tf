fbb66d4b6aac78bf96ab445db163e9b4da79337c
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:53:42 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -17,10 +17,10 @@ EOT
     loadbalancer_id     = string
     name                = string
     port                = number
-    interval_in_seconds = optional(number, 15)
-    number_of_probes    = optional(number, 2)
-    probe_threshold     = optional(number, 1)
-    protocol            = optional(string, "Tcp")
+    interval_in_seconds = optional(number) # Default: 15
+    number_of_probes    = optional(number) # Default: 2
+    probe_threshold     = optional(number) # Default: 1
+    protocol            = optional(string) # Default: "Tcp"
     request_path        = optional(string)
   }))
 }
